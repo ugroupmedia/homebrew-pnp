abcde78c1da1b1511fbddb2d594a496f6d7d2852
Yves Desgagne <yves@khome.org>
2020-02-05 05:23:17 -0500

    First version.

--- grpc-swift.rb
+++ grpc-swift.rb
@@ -3,7 +3,7 @@ class GrpcSwift < Formula
   homepage "https://github.com/grpc/grpc-swift"
   #url "https://github.com/grpc/grpc-swift/archive/0.9.1.tar.gz"
   #sha256 "6b3a2ed13c805c6b8f339f558f2a1372bdcf84c079c104a6d0b54fd7650b8fbf"
-  head "https://github.com/grpc/grpc-swift.git", :branch => "nio", :revision => "5f40a43567495b534dd445e6dd60181dff5a28d6"
+  head "https://github.com/grpc/grpc-swift.git", :revision => "5f40a43567495b534dd445e6dd60181dff5a28d6"
 
   depends_on :xcode => ["10.0", :build]
   depends_on "protobuf"
