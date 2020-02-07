43b4f103ff21af8f55cd20c47c4f8b8590432530
Yves Desgagne <yves@khome.org>
2020-02-07 14:28:59 -0500

    Added grpc-web.

--- grpc-web.rb
+++
@@ -1,8 +0,0 @@
-cask 'grpc-web' do
-  version '1.0.7'
-  sha256 'b6d399af774c567332df99746269876b13defefbeba6e0cc62ff9dfc524166c8'
-
-  url "https://github.com/ugroupmedia/homebrew-pnp/Casks/protoc-gen-grpc-web-#{version}-darwin-x86_64"
-  name 'gRPC-Web'
-  homepage 'https://github.com/grpc/grpc-web'
-end
