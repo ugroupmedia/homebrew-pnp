1589ca6d6c5aebc61940e2147e19b5392c5ba1aa
Yves Desgagne <yves@khome.org>
2020-02-10 19:37:54 -0500

    Added grpc-java

--- Casks/grpc-java.rb
+++ Casks/grpc-java.rb
@@ -2,7 +2,7 @@ cask 'grpc-java' do
   version '1.27.0'
   sha256 '2f460610fa28795b25c839cfaaa358ebd6d70aff952f9effc284d39669396c35'
 
-  url "https://search.maven.org/remotecontent?filepath=io/grpc/protoc-gen-grpc-java/#{version}/protoc-gen-grpc-java-#{version}-osx-x86_64.exe"
+  url "https://search.maven.org/remotecontent?filepath=io/grpc/protoc-gen-grpc-java/#{version}/protoc-gen-grpc-java-#{version}-osx-x86_64.exe" referer: 'https://search.maven.org/remotecontent/io/grpc/protoc-gen-grpc-java/#{version}/protoc-gen-grpc-java-#{version}-osx-x86_64.exe'
   name 'grpc-java'
   homepage 'https://github.com/grpc/grpc-java'
 
