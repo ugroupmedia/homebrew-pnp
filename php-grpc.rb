02adf3996dd6d91bcfcaa855bb9be4773ad346d9
Yves Desgagne <yves@khome.org>
2020-04-03 13:19:25 -0400

    First draft of php-grpc formula.

---
+++ php-grpc.rb
@@ -0,0 +1,18 @@
+class PhpGrpc < Formula
+  desc "Fast and furious GRPC server for PHP applications"
+  homepage "https://github.com/spiral/php-grpc"
+  url "https://github.com/spiral/php-grpc.git",
+      :tag      => "v1.2.0"
+  head "https://github.com/protocolbuffers/protobuf.git"
+
+  depends_on "go"
+  depends_on "protobuf"
+  depends_on "grpc"
+
+  def install
+    system "cd", "cmd/protoc-gen-php-grpc"
+    system "go", "get"
+    system "go", "build"
+    bin.install "protoc-gen-php-grpc"
+  end
+end
