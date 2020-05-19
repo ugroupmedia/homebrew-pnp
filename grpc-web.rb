9ddc62b8d52e7bf7f6710b7ca816e4e11b668448
Yves Desgagne <yves@khome.org>
2020-05-19 14:12:08 -0400

    grpc-web now using v1.1.0-rc.1.

---
+++ grpc-web.rb
@@ -0,0 +1,15 @@
+class GrpcWeb < Formula
+  desc "gRPC-Web provides a JavaScript library that lets browser clients access a gRPC service."
+  homepage "https://github.com/grpc/grpc-web"
+  url "https://github.com/grpc/grpc-web.git",
+      :tag      => "1.1.0-rc.1"
+  head "https://github.com/grpc/grpc-web.git"
+
+  depends_on "protobuf"
+  depends_on "grpc"
+
+  def install
+    system "make plugin"
+    bin.install "./javascript/net/grpc/web/protoc-gen-grpc-web"
+  end
+end
