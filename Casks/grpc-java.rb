6493eef4a055696c73b5377a3ef2489b0c57e4e9
Yves Desgagne <yves@khome.org>
2020-05-19 11:40:32 -0400

    grpc-web now using v1.29.

--- Casks/grpc-java.rb
+++ Casks/grpc-java.rb
@@ -1,6 +1,6 @@
 cask 'grpc-java' do
-  version '1.27.0'
-  sha256 '2f460610fa28795b25c839cfaaa358ebd6d70aff952f9effc284d39669396c35'
+  version '1.29.0'
+  sha256 'd3051d14c9e5884a0268626527a3860c04c910415250110849ab6c42e041bed8'
 
   url "https://search.maven.org/remotecontent?filepath=io/grpc/protoc-gen-grpc-java/#{version}/protoc-gen-grpc-java-#{version}-osx-x86_64.exe"
   name 'grpc-java'
