62e17c20bafcde9765992cd6d54ee02c9b053a0f
Yves Desgagne <yves@khome.org>
2020-05-26 13:01:10 -0400

    Reverted web generator to previous stable build.

--- grpc-web.rb
+++ grpc-web.rb
@@ -2,7 +2,7 @@ class GrpcWeb < Formula
   desc "gRPC-Web provides a JavaScript library that lets browser clients access a gRPC service."
   homepage "https://github.com/grpc/grpc-web"
   url "https://github.com/grpc/grpc-web.git",
-      :tag      => "1.1.0-rc.1"
+      :tag      => "1.0.7"
   head "https://github.com/grpc/grpc-web.git"
 
   depends_on "protobuf"
