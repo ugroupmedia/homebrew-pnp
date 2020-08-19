8284b1c3c3717332c21f24006fd37454369aabd8
Yves Desgagne <yves@khome.org>
2020-08-19 11:47:24 -0400

    Added patched prtotobuf.

---
+++ protobuf.rb
@@ -0,0 +1,85 @@
+class Protobuf < Formula
+  desc "Protocol buffers (Google's data interchange format)"
+  homepage "https://github.com/protocolbuffers/protobuf/"
+  url "https://github.com/protocolbuffers/protobuf/releases/download/v3.12.4/protobuf-all-3.12.4.tar.gz"
+  sha256 "e7bf41873d1a87c05c2b0a6197f4445c6ea3469ce0165ff14de2df8b34262530"
+  license "BSD-3-Clause"
+
+  bottle do
+    cellar :any
+    sha256 "a18a7e12c9effa966240e3123ccd6d222b3663ae0399e48d00cb6c61bdae63f4" => :catalina
+    sha256 "20327eb69135b303d3b54d1d6d9d386a60e2499b75118a8b27ab0db40ccfb27a" => :mojave
+    sha256 "0e280b162595b248676e0070d768f8d5784c20322addcc3912bd3f0002e046af" => :high_sierra
+  end
+
+  head do
+    url "https://github.com/protocolbuffers/protobuf.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "python@3.8" => [:build, :test]
+
+  patch do
+    url "https://github.com/protocolbuffers/protobuf/pull/7379.patch"
+    sha256 "a1010aec3d4c0216d0eec6f41e5e39bf874d5f1c0d775f0c4d36baf7daa67f5f"
+  end
+
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/6b/34/415834bfdafca3c5f451532e8a8d9ba89a21c9743a0c59fbd0205c7f9426/six-1.15.0.tar.gz"
+    sha256 "30639c035cdb23534cd4aa2dd52c3bf48f06e5f4a941509c8bafd8ce11080259"
+  end
+
+  def install
+    # Don't build in debug mode. See:
+    # https://github.com/Homebrew/homebrew/issues/9279
+    # https://github.com/protocolbuffers/protobuf/blob/5c24564811c08772d090305be36fae82d8f12bbe/configure.ac#L61
+    ENV.prepend "CXXFLAGS", "-DNDEBUG"
+    ENV.cxx11
+
+    system "./autogen.sh" if build.head?
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}", "--with-zlib"
+    system "make"
+    system "make", "check"
+    system "make", "install"
+
+    # Install editor support and examples
+    pkgshare.install "editors/proto.vim", "examples"
+    elisp.install "editors/protobuf-mode.el"
+
+    ENV.append_to_cflags "-I#{include}"
+    ENV.append_to_cflags "-L#{lib}"
+
+    resource("six").stage do
+      system Formula["python@3.8"].opt_bin/"python3", *Language::Python.setup_install_args(libexec)
+    end
+    chdir "python" do
+      system Formula["python@3.8"].opt_bin/"python3", *Language::Python.setup_install_args(libexec),
+                        "--cpp_implementation"
+    end
+
+    version = Language::Python.major_minor_version Formula["python@3.8"].opt_bin/"python3"
+    site_packages = "lib/python#{version}/site-packages"
+    pth_contents = "import site; site.addsitedir('#{libexec/site_packages}')\n"
+    (prefix/site_packages/"homebrew-protobuf.pth").write pth_contents
+  end
+
+  test do
+    testdata = <<~EOS
+      syntax = "proto3";
+      package test;
+      message TestCase {
+        string name = 4;
+      }
+      message Test {
+        repeated TestCase case = 1;
+      }
+    EOS
+    (testpath/"test.proto").write testdata
+    system bin/"protoc", "test.proto", "--cpp_out=."
+    system Formula["python@3.8"].opt_bin/"python3", "-c", "import google.protobuf"
+  end
+end
