bf22a781b6d5271aae98aeb636f7230c3fb32708
Yves Desgagné <yves@khome.org>
2020-09-09 13:02:54 -0400

    grpc-swift from 1.0.0-alpha.14 to 1.0.0-alpha.19

--- grpc-swift.rb
+++ grpc-swift.rb
@@ -1,9 +1,9 @@
 class GrpcSwift < Formula
   desc "The Swift language implementation of gRPC"
   homepage "https://github.com/grpc/grpc-swift"
-  url "https://github.com/grpc/grpc-swift/archive/1.0.0-alpha.14.tar.gz"
-  sha256 "d2beb362e4748c4e724b73499ea3ff743a61dbf67ead82558568a75776e4a6c7"
-  # head "https://github.com/grpc/grpc-swift.git", :revision => "5f40a43567495b534dd445e6dd60181dff5a28d6"
+  url "https://github.com/grpc/grpc-swift.git",
+      :tag      => "1.0.0-alpha.19"
+  head "https://github.com/grpc/grpc-swift.git"
 
   depends_on :xcode => ["10.0", :build]
   depends_on "protobuf"
