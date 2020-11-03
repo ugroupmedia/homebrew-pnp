d3d66b7ac79768ee81d6b317ea26231ccdd47423
Yves Desgagne <yves@khome.org>
2020-11-03 10:36:23 -0500

    Added patch for php-grpc.

--- php-grpc.rb
+++ php-grpc.rb
@@ -2,12 +2,17 @@ class PhpGrpc < Formula
   desc "Fast and furious GRPC server for PHP applications"
   homepage "https://github.com/spiral/php-grpc"
   url "https://github.com/spiral/php-grpc.git",
-      :tag      => "v1.4.0"
+      :tag      => "v1.2.0"
   head "https://github.com/protocolbuffers/protobuf.git"
 
   depends_on "go"
   depends_on "protobuf"
   depends_on "grpc"
+
+  patch do
+    url "https://github.com/supersmile2009/php-grpc/commit/dab727e8a5fe96a46378a0f459ce4d7893ab8aae.patch"
+    sha256 "7b61d18a858a8e90a94ace0244861d511d7240c29d766c3f0db40e2c8a16e8f6"
+  end
 
   def install
     system "./build.sh", "Mac"
