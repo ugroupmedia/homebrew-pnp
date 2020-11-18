b961b1e13437d8c30a4392d7e8d08ec0977a8708
Dick Smith <github@dicksmith.ca>
2020-11-18 08:42:42 -0500

    Update grpc-java.rb

--- Casks/grpc-java.rb
+++ Casks/grpc-java.rb
@@ -1,6 +1,6 @@
 cask 'grpc-java' do
   version '1.33.1'
-  sha256 '86fb2497f7522c2ee41ccfd77f67ef8c2a65be54ad2550493ca15d1ca3b73b17'
+  sha256 'ac3c8b12370329d897a896dd99e0a81f8463b5cd5b7d2014c78085bf5841f20a'
 
   url "https://search.maven.org/remotecontent?filepath=io/grpc/protoc-gen-grpc-java/#{version}/protoc-gen-grpc-java-#{version}-osx-x86_64.exe"
   name 'grpc-java'
