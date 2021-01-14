9e8e58d265ac6bdaf1259cb5ac9ff8172741aa1e
Yves Desgagne <yves@khome.org>
2021-01-14 13:00:08 -0500

    Updated to grpc-java 1.35.0

--- Casks/grpc-java.rb
+++ Casks/grpc-java.rb
@@ -1,6 +1,6 @@
 cask 'grpc-java' do
-  version '1.33.1'
-  sha256 'ac3c8b12370329d897a896dd99e0a81f8463b5cd5b7d2014c78085bf5841f20a'
+  version '1.35.0'
+  sha256 'bda36b943a9f427f4c1e085d1b22f6e3c23d6014ae7f91c57be128ea698c042d'
 
   url "https://search.maven.org/remotecontent?filepath=io/grpc/protoc-gen-grpc-java/#{version}/protoc-gen-grpc-java-#{version}-osx-x86_64.exe"
   name 'grpc-java'
