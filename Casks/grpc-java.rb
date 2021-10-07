d093046abc83f7eb136db1dd7a729d4aac3327ff
Franck CLEMENT <franck.clement@ugroupmedia.com>
2021-10-07 10:54:33 -0400

    grpc-java bump from 1.39.0 to 1.41.0

--- Casks/grpc-java.rb
+++ Casks/grpc-java.rb
@@ -1,6 +1,6 @@
 cask 'grpc-java' do
-  version '1.39.0'
-  sha256 '2aa5e940922d2acd86601f80b1d8c23fec71c92b6f519b6f0f5800d8e3a62062'
+  version '1.41.0'
+  sha256 'f92859e25646f0db33b9b542a07053ebf2569304abf18338c9e6f75ec8993b0d'
 
   url "https://search.maven.org/remotecontent?filepath=io/grpc/protoc-gen-grpc-java/#{version}/protoc-gen-grpc-java-#{version}-osx-x86_64.exe"
   name 'grpc-java'
