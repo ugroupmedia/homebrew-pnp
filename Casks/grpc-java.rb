9126fc909d831e4556cd426002b90df55557f7ca
Franck CLEMENT <franck.clement@ugroupmedia.com>
2023-07-12 13:22:26 -0400

    Updated grpc-java from 1.47.0 to 1.56.1

--- Casks/grpc-java.rb
+++ Casks/grpc-java.rb
@@ -1,6 +1,6 @@
 cask 'grpc-java' do
-  version '1.47.0'
-  sha256 'bfc05f91b8502888da995739290ce82f5269290f806f9c58a5bc986f814d9aa0'
+  version '1.56.1'
+  sha256 '1875e3035f03bd3dd457c24ed8aa9d132ef1a3d06432606fbe1fdb7589a3014f'
 
   url "https://search.maven.org/remotecontent?filepath=io/grpc/protoc-gen-grpc-java/#{version}/protoc-gen-grpc-java-#{version}-osx-x86_64.exe"
   name 'grpc-java'
