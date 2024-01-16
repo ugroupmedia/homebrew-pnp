6cc86eafc0ca82e4af971a031950320f327f700e
Franck Clement <franck.clement@ugroupmedia.com>
2024-01-16 10:59:14 -0500

    Updated grpc-java from 1.56.1 to 1.61.0 and grpc-swift from 1.18.0 to 1.21.0

--- Casks/grpc-java.rb
+++ Casks/grpc-java.rb
@@ -1,6 +1,6 @@
 cask 'grpc-java' do
-  version '1.56.1'
-  sha256 '1875e3035f03bd3dd457c24ed8aa9d132ef1a3d06432606fbe1fdb7589a3014f'
+  version '1.61.0'
+  sha256 'd865fdcadf5d1dbda42e0c97ad4207e30da4c1e01bf7c02ae014521de2f5ae6d'
 
   url "https://search.maven.org/remotecontent?filepath=io/grpc/protoc-gen-grpc-java/#{version}/protoc-gen-grpc-java-#{version}-osx-x86_64.exe"
   name 'grpc-java'
